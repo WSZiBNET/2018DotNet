8a50683c619dbf32b243c41697b6a53afc97632a
mitupta <michal.tupta@gmail.com>
2019-03-23 16:08:33 +0100

    Adding opinion class

---
+++ Grupa4/ClientServiceApp/ClientServiceApp/Models/Opinion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientServiceApp.Models
+{
+    public class Opinion
+    {
+        public int Id { get; set; }
+        public string OpinionText { get; set; }
+
+        public int Rate { get; set; }
+
+        public Employee Employee { get; set; }
+
+        public int EmployeeId { get; set; }
+        public Client Client { get; set; }
+        public int ClientId { get; set; }
+
+        public DateTime OpinionDate { get; set; }
+    }
+}
