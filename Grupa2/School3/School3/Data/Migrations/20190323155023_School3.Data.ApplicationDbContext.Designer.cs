8a56f7daf635ef57065dc9fbdd25aa329d4d2b03
panczi <panek.mt@gmail.com>
2019-03-23 16:52:08 +0100

    app setings plus migration not working

--- Grupa2/School3/School3/Data/Migrations/20190323155023_School3.Data.ApplicationDbContext.Designer.cs
+++ Grupa2/School3/School3/Data/Migrations/20190323155023_School3.Data.ApplicationDbContext.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace School3.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190323155023_School3.Data.ApplicationDbContext")]
+    partial class School3DataApplicationDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
