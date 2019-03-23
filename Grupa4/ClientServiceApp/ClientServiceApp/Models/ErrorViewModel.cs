d1bce4a0f319998e2e7497028911e8c5f6813514
sadamski <sadamski@KOMP409-08>
2019-03-23 14:33:36 +0100

    Initial commit

---
+++ Grupa4/ClientServiceApp/ClientServiceApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClientServiceApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
