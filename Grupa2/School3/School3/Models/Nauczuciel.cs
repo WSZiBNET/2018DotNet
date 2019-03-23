1b0225a072018477d773fbce8422dcd55eee5b59
dtrebacz <takijeden@autograf.pl>
2019-03-23 16:40:36 +0100

    klasy

--- Grupa2/School3/School3/Models/Nauczuciel.cs
+++ Grupa2/School3/School3/Models/Nauczuciel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace School3.Models
 {
     public class Nauczuciel
     {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Przedmiot { get; set; }
+
+
     }
 }
