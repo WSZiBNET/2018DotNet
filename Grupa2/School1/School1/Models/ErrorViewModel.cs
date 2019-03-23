ceaa18db9bd80a84724cf6f3e15450256404f5ca
pwarszaw <pwarszaw@wszib.edu.pl>
2019-03-23 14:42:05 +0100

    Dodanie solucji

---
+++ Grupa2/School1/School1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace School1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
