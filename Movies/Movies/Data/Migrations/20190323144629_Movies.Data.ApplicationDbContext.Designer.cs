0640208db4e5e464010780515408a32ea902c35d
lukasz82 <maxus_pl@o2.pl>
2019-03-23 16:07:04 +0100

    Migracje

--- Movies/Movies/Data/Migrations/20190323144629_Movies.Data.ApplicationDbContext.Designer.cs
+++ Movies/Movies/Data/Migrations/20190323144629_Movies.Data.ApplicationDbContext.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Movies.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190323144629_Movies.Data.ApplicationDbContext")]
+    partial class MoviesDataApplicationDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
