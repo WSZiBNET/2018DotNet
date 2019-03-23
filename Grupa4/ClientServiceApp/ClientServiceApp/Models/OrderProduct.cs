d2733f4dbf113fd07ca06c8988067c3f16792b0d
golenia <golenius@gmail.com>
2019-03-23 16:10:57 +0100

    Adding orderproduct, orderserice class

---
+++ Grupa4/ClientServiceApp/ClientServiceApp/Models/OrderProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientServiceApp.Models
+{
+    public class OrderProduct
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public int ProductId { get; set; }
+        public DateTime SaleDate { get; set; }
+        public Client Client { get; set; }
+        public int ClientId { get; set; }
+        public Employee Seller { get; set; }
+        public int SellerId { get; set; }
+        public int Amount { get; set; }
+
+
+    }
+}
---
+++ Grupa4/ClientServiceApp/ClientServiceApp/Models/OrderService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientServiceApp.Models
+{
+    public class OrderService
+    {
+
+        public int Id { get; set; }
+        public Service Service { get; set; }
+        public int ServiceId { get; set; }
+        public DateTime SaleDate { get; set; }
+        public Client Client { get; set; }
+        public int ClientId { get; set; }
+        public Employee Seller { get; set; }
+        public int SellerId { get; set; }
+        public int Amount { get; set; }
+    }
+}
