dce2e70e29df9508ca653f8775593c163e7eb061
daniel@groupka.pl <daniel@groupka.pl>
2019-03-24 13:24:54 +0100

    Poprawna wersja

---
+++ ASPWlasciwosci/ASPWlasciwosci/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPWlasciwosci.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [Authorize(Policy = "WymagajManagera")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
