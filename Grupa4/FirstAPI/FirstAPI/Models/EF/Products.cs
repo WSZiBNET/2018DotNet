68d28bdb0623018a3b025f11a90deeca01a1dc4a
rkorpo <45516192+rkorpo@users.noreply.github.com>
2019-04-07 09:02:43 +0200

    Added API

---
+++ Grupa4/FirstAPI/FirstAPI/Models/EF/Products.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstAPI.Models.EF
+{
+    public partial class Products
+    {
+        public int Id { get; set; }
+        public int Category { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
